b476b59e66263c8f062c8bfb3eb6f43a11e824df
baratN <bharathtnf@gmail.com>
2019-02-24 23:56:33 +0530

    Added new Libraries

---
+++ WebApi/EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataLibrary;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> GET()
+        {
+            using (EmployeeDBEntity entities=new EmployeeDBEntity())
+            {
+                return entities.
+            }
+        }
+    }
+}
--- WebApi/WebApi/Controllers/ValuesController.cs
+++ WebApi/WebApi/Controllers/ValuesController.cs
@@ -9,31 +9,40 @@ namespace WebApi.Controllers
 {
     public class ValuesController : ApiController
     {
+
+        public static List<string> strings = new List<string>()
+        {
+            "value0","value1","value2"
+        };
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "bharath", "appu" };
+            return strings;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "bharath";
+            return strings[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            strings.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            strings[id]= value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
     }
 }
